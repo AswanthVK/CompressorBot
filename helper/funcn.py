b6879b9639d1a043da7239371cfbd8aba5652896
Aswanth V K <55796045+AswanthVK@users.noreply.github.com>
2021-05-29 17:48:23 +0530

    Update funcn.py

--- helper/funcn.py
+++ helper/funcn.py
@@ -85,7 +85,7 @@ async def progress(current, total, event, start, type_of_ps, file=None):
         )
         tmp = (
             progress_str
-            + "Done: {0} of {1}\nSpeed: {2}/s\n\nETA: {3}\n".format(
+            + "**Done:** {0} of {1}\n**Speed:** {2}/s\n**ETA:** {3}\n".format(
                 hbs(current),
                 hbs(total),
                 hbs(speed),
